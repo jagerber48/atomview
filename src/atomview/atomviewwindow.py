6ea68c7947d20c1b588fb3205b20902e9012ae7e
Justin <justin.gerber48@gmail.com>
2023-09-23 11:05:48 -0600

    Update UI

--- src/atomview/atomviewwindow.py
+++ src/atomview/atomviewwindow.py
@@ -1,4 +1,4 @@
-import sys
+from enum import Enum
 
 from PyQt6 import QtWidgets, QtCore
 from pyvistaqt import MainWindow
@@ -6,6 +6,11 @@ from pyvistaqt import MainWindow
 from atomview.ui_atomviewwindow import Ui_AtomViewWindow
 from atomview.atom_wavefunction import get_wavefunction_prob_contour_mesh, \
     get_wavefunction_volume_mesh
+
+
+class VisMode(Enum):
+    CONTOUR = 'contour'
+    VOLUME = 'volume'
 
 
 class AtomViewWindow(MainWindow):
@@ -22,9 +27,9 @@ class AtomViewWindow(MainWindow):
         self.n = 1
         self.l = 0
         self.m = 0
-        self.real = self.ui.real_checkBox.isChecked()
-        self.clip = self.ui.clip_checkBox.isChecked()
-        self.contour_mode = self.ui.contour_radioButton.isChecked()
+        self.real = self.ui.real_radioButton.isChecked()
+        self.cutout = self.ui.cutout_checkBox.isChecked()
+        self.vis_mode = self.get_vis_mode()
 
         self.ui.plotter.camera.position = (10, 10, 10)
         self.ui.plotter.set_background('black')
@@ -34,42 +39,52 @@ class AtomViewWindow(MainWindow):
         self.ui.l_comboBox.activated.connect(self.update_l)
         self.ui.m_comboBox.activated.connect(self.update_m)
 
-        self.ui.real_checkBox.stateChanged.connect(self.update_real)
-        self.ui.clip_checkBox.stateChanged.connect(self.update_clip)
+        self.ui.real_complex_buttonGroup.buttonToggled.connect(self.update_real)
+        self.ui.cutout_checkBox.stateChanged.connect(self.update_cutout)
 
-        self.ui.buttonGroup.buttonClicked.connect(self.update_mode)
+        self.ui.mode_tabWidget.currentChanged.connect(self.update_vis_mode)
 
         self.show()
 
-    def update_mode(self):
-        self.contour_mode = self.ui.contour_radioButton.isChecked()
+    def get_vis_mode(self):
+        if self.ui.mode_tabWidget.currentIndex() == 0:
+            return VisMode.CONTOUR
+        elif self.ui.mode_tabWidget.currentIndex() == 1:
+            return VisMode.VOLUME
+        else:
+            raise ValueError
+
+    def update_vis_mode(self):
+        self.vis_mode = self.get_vis_mode()
         self.update_mesh()
 
     def update_mesh(self):
         camera_position = self.ui.plotter.camera.position
         self.ui.plotter.clear_actors()
 
-        if self.contour_mode:
+        if self.vis_mode is VisMode.CONTOUR:
             mesh = get_wavefunction_prob_contour_mesh(self.n, self.l, self.m,
                                                       num_pts=100,
                                                       real=self.real,
-                                                      clip=self.clip)
+                                                      clip=self.cutout)
             self.ui.plotter.add_mesh(mesh, scalars='rgba', rgb=True,
                                      specular=1, diffuse=1, ambient=0.3)
-        else:
+        elif self.vis_mode is VisMode.VOLUME:
             mesh = get_wavefunction_volume_mesh(self.n, self.l, self.m,
                                                 num_pts=100, real=self.real,
                                                 max_opacity=1)
             self.ui.plotter.add_volume(mesh, scalars='rgba', mapper='gpu')
+        else:
+            raise NotImplementedError
 
         self.ui.plotter.camera.position = camera_position
 
     def update_real(self):
-        self.real = self.ui.real_checkBox.isChecked()
+        self.real = self.ui.real_radioButton.isChecked()
         self.update_mesh()
 
-    def update_clip(self):
-        self.clip = self.ui.clip_checkBox.isChecked()
+    def update_cutout(self):
+        self.cutout = self.ui.cutout_checkBox.isChecked()
         self.update_mesh()
 
     def update_n(self):
@@ -101,11 +116,3 @@ class AtomViewWindow(MainWindow):
     def update_m(self):
         self.m = int(self.ui.m_comboBox.currentText())
         self.update_mesh()
-
-
-if __name__ == '__main__':
-    app = QtCore.QCoreApplication.instance()
-    if app is None:
-        app = QtWidgets.QApplication(sys.argv)
-    window = AtomViewWindow()
-    app.exec()
---
+++ ui/convert_ui_to_py.bat
@@ -0,0 +1,4 @@
+CALL ..\.venv\Scripts\activate
+pyuic6 ui_atomviewwindow.ui -o ..\src\atomview\ui_atomviewwindow.py
+
+cmd /k
